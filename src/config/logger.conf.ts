fc1100f311ab03ab72650e6227ec6bc2c083a582
Michele Lorenzoni <michelelorenzoni321@gmail.com>
2025-05-22 00:23:13 +0200

    REFACTOR: added config for test environment for pino

--- src/config/logger.conf.ts
+++ src/config/logger.conf.ts
@@ -20,6 +20,36 @@ export const devConf: LoggerOptions = {
   level: "debug",
 };
 
+// config for test env
+export const testConf: LoggerOptions = {
+  transport: {
+    targets: [
+      {
+        target: "pino/file",
+        level: "debug",
+        options: {
+          destination: path.join(LOGGER.BASE_DIRECTORY, "url-shortener.log"),
+          mkdir: true,
+          sync: false,
+        },
+      },
+      {
+        target: "pino/file",
+        level: "debug",
+        options: {
+          destination: path.join(LOGGER.BASE_DIRECTORY, "url-shortener.err.log"),
+          mkdir: true,
+          sync: false,
+        },
+      },
+    ],
+  },
+  level: "debug",
+  timestamp: (): string => `,"timestamp":"${new Date().toISOString()}"`,
+  // Removed the formatters since they're not compatible with transport targets
+  messageKey: "message",
+};
+
 // config for production env
 export const prodConf: LoggerOptions = {
   transport: {
